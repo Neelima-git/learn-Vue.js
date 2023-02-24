25a77760cb46590f09b0e775f437290a0fe91977
nbellana <nbellana@innominds.com>
2023-02-24 16:47:52 +0530

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- String Interpolation:
+    This is one way of showing data in template.
+  Class "app" adds bg-pink as defined in styles-->
+  <div class="app">
+    Hello {{ name }}
+  </div>
+  <!-- Directive: v-text
+    This is another way of showing data in the template using directive "v-text" -->
+  <div v-text="name"></div>
+  <!-- uses string interpolation to display the value of the "country" data property. -->
+  <div>{{ country }}</div>
+  <!-- uses the "v-html" directive to display the value of the "country" data property as HTML content. 
+  In this case, the HTML content is bold text "From India" wrapped in the "b" tag.-->
+  <div v-html="country"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
+  // Each component should have a name. First letter should be capital.
   name: 'App',
-  components: {
-    HelloWorld
+  //This data is similar useState(), we just maintain our components state using useState()
+  //So here we'll maintain local state of data.
+  data(){
+    return{
+      name: 'Neeluu',
+      country: '<b>From India</b>'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .app{
+    background-color: pink;
+  }
 </style>
