9d8928a665cbac0d1758d83d819a7c0b06f3bc39
neelima-git <bellana.neelima2@gmail.com>
2023-03-06 12:49:44 +0530

    added component registration code

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
-<!-- This is a Vue.js component that allows the user to input two numbers and calculates their sum using a computed property. -->
-
-<script>
-export default {
-  // Set the name of the component to "Counter_O"
-  name: "Counter_O",
-  // Define the data properties for the component
-  data() {
-    // Initialize the "num1" and "num2" properties to 10 and 20, respectively
-    return { num1: 10, num2: 20 };
-  },
-  // Define the methods for the component
-  methods: {
-    // This method doesn't seem to be related to the counter, as it just sets the name property to "value"
-    Add() {
-      this.name = "value";
-    },
-  },
-  // Define the computed properties for the component
-  computed: {
-    // Define a computed property called "sum" that calculates the sum of "num1" and "num2"
-    sum() {
-      return this.num1 + this.num2;
-    },
-  },
-};
-</script>
-
-<!-- Define the template for the component -->
-
 <template>
-  <!-- Define two input fields that allow the user to input numbers, and bind them to the "num1" and "num2" data properties using the v-model directive -->
-  <input type="number" v-model="num1" /> <br />
-  <input type="number" v-model="num2" /> <br />
-  <!-- Define an input field that displays the sum of "num1" and "num2", and bind it to the "sum" computed property using the v-model directive -->
-  <input type="number" v-model="sum" />
+  <h3>Parent component</h3>
+  <ChildComponent/>
 </template>
 
-<!-- Define any custom styles for the component (none are used in this case) -->
-<style></style>
+<script>
+
+// import DataBinding from './components/DataBinding.vue';
+import ChildComponent from './components/ComponentRegistration.vue'
+
+export default {
+  components: {
+    ChildComponent
+  }
+}
+</script>ComponentRegistration
+<style>
+
+</style>
---
+++ src/components/ComponentRegistration.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3 v-bind:style={color:headingColor}>Inside the child component</h3>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data(){
+        return{
+            headingColor: 'pink'
+        }
+    }
+}
+</script>
