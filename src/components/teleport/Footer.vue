6ac480ed3345929b557786c8d177636ea7ab045b
neelima-git <bellana.neelima2@gmail.com>
2023-03-26 23:24:12 +0530

    added teleport demo

---
+++ src/components/teleport/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>Footer of App</h3>
+    <p>Copy rights</p>
+</template>
+<script>
+export default {
+    name: "FooterComponent",
+}
+</script>
---
+++ src/components/teleport/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Teleport components</h1>
+  <teleport to="#footer"> 
+    <Footer />
+  </teleport>
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+export default {
+  name: "TeleportComponent",
+  components: {
+    Footer,
+  },
+};
+</script>
