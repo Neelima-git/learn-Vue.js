e796a57bdcb3e4ac563f0865b1dcec0bdd562fb8
neelima-git <bellana.neelima2@gmail.com>
2023-03-30 23:54:47 +0530

    added comments to props component

--- src/components/props/CtoP_Child.vue
+++ src/components/props/CtoP_Child.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
-        <button v-on:click="updateTitle">Update title</button>
+      <!-- Render a button and listen to the click event -->
+      <button v-on:click="updateTitle">Update title</button>
     </div>
-</template>
-<script>
-export default{
-    name: 'CtoP_Child',
-    methods:{
-        updateTitle(){
-            console.warn("Update title");
-            this.$emit('changeTitle', 'Child to parent props passed')
-        }
-    }
-}
-</script>
+  </template>
+  
+  <script>
+  export default {
+    // Set the component name
+    name: "CtoP_Child", 
+    methods: {
+      updateTitle() {
+        // Log a warning message to the console
+        console.warn("Update title"); 
+        // Emit a 'changeTitle' event and pass the string 'Child to parent props passed' as an argument
+        this.$emit("changeTitle", "Child to parent props passed"); 
+      },
+    },
+  };
+  </script>
+  
--- src/components/props/CtoP_Parent.vue
+++ src/components/props/CtoP_Parent.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
+    <!-- Display the title property in an h3 tag -->
     <h3>{{ title }}</h3>
+    <!-- Render the Child component and listen to the changeTitle event -->
     <Child v-on:changeTitle="updateTitle($event)" />
   </div>
 </template>
 
 <script>
-import Child from "../CtoP_Child.vue";
+import Child from "../CtoP_Child.vue"; // Import the Child component
 
 export default {
-  name: "CtoP_Parent",
+  name: "CtoP_Parent", // Set the component name
   components: {
-    Child,
+    Child, // Register the Child component
   },
   data() {
     return {
-      title: "Demo of passing props from child to parent component",
+      title: "Demo of passing props from child to parent component", // Initialize the title property
     };
   },
   methods: {
     updateTitle(title) {
-      this.title = title;
+      this.title = title; // Update the title property with the new value received from the Child component
     },
   },
 };
