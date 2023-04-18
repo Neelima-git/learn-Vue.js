1f41e3e20db15dc7ca1e2ebc72731cdb90de3081
neelima-git <bellana.neelima2@gmail.com>
2023-04-18 23:51:59 +0530

    .

--- src/components/routing/Profile.vue
+++ src/components/routing/Profile.vue
@@ -1,9 +1,20 @@
 <template>
-    <h1>Profile</h1>
+    <h1>{{ profile }} Profile</h1>
 </template>
 <script>
+import { useRoute } from 'vue-router';
 export default {
-    name: "ProfileComponent"
+    name: "ProfileComponent",
+    data(){
+        return {
+            profile: ""
+        }
+    },
+    mounted(){
+        const route = useRoute();
+        console.log("route", route.params.name);
+        this.profile=route.params.name;
+    }
 }
 </script>
 <style>
--- src/components/routing/RoutesDemo.vue
+++ src/components/routing/RoutesDemo.vue
@@ -1,8 +1,10 @@
 <template>
     <router-link to="/">Home</router-link>
     <router-link to="/login">Login</router-link>
-    <router-link to="/profile">Profile</router-link>
-    <router-view />
+    <router-link to="/profile/peter">Peters Profile</router-link>
+    <router-link to="/profile/bruce">Bruces Profile</router-link>
+    <router-link to="/profile/anil">Anils Profile</router-link>
+    <router-view></router-view>
 </template>
 <script>
 export default {
