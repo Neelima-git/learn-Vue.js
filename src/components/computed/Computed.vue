e7c546f89d9ea9e4dbe53ab41c1cf4d755c03ff4
neelima-git <bellana.neelima2@gmail.com>
2023-05-17 23:44:47 +0530

    .

--- src/components/computed/Computed.vue
+++ src/components/computed/Computed.vue
@@ -1,6 +1,8 @@
 <template>
+    <!-- Displaying the computed property -->
     <h3>Computed property</h3>
     <h2>{{ getTotal }}</h2>
+    <!-- Calling the getData method -->
     <h2>{{ getData() }}</h2>
 </template>
 
@@ -9,21 +11,31 @@ export default {
     name: "ComputedComponent",
     data(){
         return {
+            // Initializing data properties
             fruitCake: 40,
             nutsBiscuit: 60,
             kitkat: 45
         }
     },
     methods: {
+        // Method to calculate and return the data
         getData() {
             return (this.fruitCake*3 + this.nutsBiscuit*2 + this.kitkat)
         }
     },
-    //Computed properties are good for performance. They first check cached res and will only compute if the res doesn't exist.
     computed: {
+        // Computed property for calculating the total
+        // Computed properties are good for performance.
+        // They first check cached result and will only compute if the result doesn't exist.
         getTotal(){
             return (this.fruitCake*3 + this.nutsBiscuit*2 + this.kitkat)
         }
     }
 }
 </script>
+
+<!-- 
+    NOTE: 
+    1. should not make any API calls within the computed property 
+    2. methods are called with parenthesis but computed are called without parenthesis.
+-->
--- src/components/lifecycleMethods/create.vue
+++ src/components/lifecycleMethods/create.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- Displaying the name property -->
     <div>
         <p>{{ name }}</p>
     </div>
@@ -9,14 +10,19 @@ export default {
     name: 'beforeCreate',
     data() {
         return{
+            // Initializing the name data property
             name: "neelu"
         }
     },
     created(){
+        // Updating the name property in the created lifecycle hook
         this.name = "neelu"
+        // Logging a warning message in the console
         console.warn("created", this.name);
     },
     beforeCreate(){
+        // Executed before the component is created
+        // Logging a warning message in the console
         console.warn("before create", this.name);
     }
 }
