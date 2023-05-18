a7d07ef7343afa8dbff6c04b84c8f43bee397bc6
neelima-git <bellana.neelima2@gmail.com>
2023-05-18 22:56:13 +0530

    added comments for slots

--- src/components/slots/Slot.vue
+++ src/components/slots/Slot.vue
@@ -1,6 +1,9 @@
+<!-- Slot.vue -->
 <template>
+    <!-- Defining a slot with a default value -->
     <slot><h2>Default value</h2></slot>
 </template>
+
 <script>
 export default {
     name: "SlotChild"
--- src/components/slots/SlotParent.vue
+++ src/components/slots/SlotParent.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
         <h1>Slot demo</h1>
+        
+        <!-- Using the SlotDemo component with default slot content -->
         <SlotDemo></SlotDemo>
+        
+        <!-- Using the SlotDemo component with custom slot content -->
         <SlotDemo>
             <h1>Able to see h1</h1>
         </SlotDemo>
+        
+        <!-- Using the SlotDemo component with different custom slot content -->
         <SlotDemo>
             <h2>Able to see h2</h2>
         </SlotDemo>
+        
+        <!-- Using the SlotDemo component with an image as slot content -->
         <SlotDemo>
             <img src="https://cdn.pixabay.com/photo/2023/03/16/08/57/cormorant-7856235_960_720.jpg" alt="bird">
         </SlotDemo>
